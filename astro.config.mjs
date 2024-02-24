a322012ca7ec2c71d53e95a8a019dcfc5ecb3759
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-02-24 10:19:42 -0500

    add apiOptions to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "us",
+      },
     }),
     tailwind(),
   ],
