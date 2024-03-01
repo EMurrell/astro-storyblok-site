22fc289f0d22debe302af1e5d3b0fc63f26dc301
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-03-01 11:16:48 -0500

    add hover animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      boxShadow: {
+        bottom: "inset 0px -10px 0px #14b8a6",
+      },
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
         serif: ["Noto Serif", ...defaultTheme.fontFamily.serif],
