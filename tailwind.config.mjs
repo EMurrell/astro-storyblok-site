4eb3ecd78874823dc2b5383827db2f9f698160f1
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-03-05 13:52:13 -0500

    add tailwind-forms plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,5 +20,8 @@ export default {
       }
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms'),
+  ],
 };
