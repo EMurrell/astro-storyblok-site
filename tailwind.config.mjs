8f06e5b2f61422c68fab4c4a77b9cf752d6183e0
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-03-05 11:45:49 -0500

    add bg images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,14 +4,23 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        pattern_1: "url(https://res.cloudinary.com/dbobkzvku/image/upload/v1709655265/Looper-1_bkgdsj.svg)",
+        pattern_2: "url(https://res.cloudinary.com/dbobkzvku/image/upload/v1709655325/Looper-2_oydwae.svg)", 
+        pattern_3: "url(https://res.cloudinary.com/dbobkzvku/image/upload/v1709655325/Looper-3_uz0lkd.svg)",
+        pattern_4: "url(https://res.cloudinary.com/dbobkzvku/image/upload/v1709655286/Looper-4_s6q0c1.svg)", 
+      },
       boxShadow: {
-        bottom: "inset 0px -10px 0px #14b8a6",
+        bottom: "inset 0px -10px 0px #fde047",
       },
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
         serif: ["Noto Serif SC", ...defaultTheme.fontFamily.serif],
         space: ["Space Grotesk Variable", ...defaultTheme.fontFamily.serif],
       },
+      animation: {
+        'spin-slow': 'spin 48s linear infinite',
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
